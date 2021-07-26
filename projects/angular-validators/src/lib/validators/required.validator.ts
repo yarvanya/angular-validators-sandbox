8fdb52fc2745c64c69d96893ac75887cc6269dae
Maksym Syniuk <msyniuk@reenbit.com>
2021-07-26 23:21:07 +0300

    Adjust required validator with handling 'false' value for checkboxes

--- projects/angular-validators/src/lib/validators/required.validator.ts
+++ projects/angular-validators/src/lib/validators/required.validator.ts
@@ -4,17 +4,19 @@ import {IMRequiredErrorInterface} from '../interfaces/required-error.interface';
 export class IMRequiredValidatorConfigModel {
   public isTrimValueRequired?: boolean;
   public isZeroAllowed?: boolean;
+  public isCheckboxShouldBeChecked?: boolean;
 }
 
 export const IMRequiredValidator = (config: IMRequiredValidatorConfigModel = new IMRequiredValidatorConfigModel()): ValidatorFn => {
   return (control: AbstractControl): ValidationErrors => {
-    if (control.value || control.value === 0) {
+    if (control.value || control.value === 0 || control.value === false) {
       switch (typeof control.value) {
         case 'string':
-          return config.isTrimValueRequired
-            ? control.value.trim() ? null : {fieldRequiredError: true} as IMRequiredErrorInterface : null;
+          return config.isTrimValueRequired ? control.value.trim() ? null : {fieldRequiredError: true} : null;
         case 'number':
-          return control.value === 0 && !config.isZeroAllowed ? {fieldRequiredError: true} : null as IMRequiredErrorInterface;
+          return control.value === 0 && !config.isZeroAllowed ? {fieldRequiredError: true} : null;
+        case 'boolean':
+          return control.value === false && config.isCheckboxShouldBeChecked ? {fieldRequiredError: true} : null;
         default: return null;
       }
     } else {
